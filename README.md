2e4ce678f1db86bf7925450d68b7687b8be50a57
clinton <clinton.freeman@nicta.com.au>
2014-10-27 21:17:48 +1000

    Test setup for clarity in transit.

---
+++ README.md
@@ -0,0 +1,4 @@
+ant debug
+
+adb install -r bin/TriggerTracker-debug.apk
+adb logcat *:W
--- src/org/triggertracker/TriggerService.java
+++ src/org/triggertracker/TriggerService.java
@@ -27,12 +27,16 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
 import com.estimote.sdk.Region;
 import com.estimote.sdk.BeaconManager;
@@ -44,7 +48,7 @@ import org.triggertracker.locationservices.TriggerLocation;
 public class TriggerService extends Service implements LocationListener {
 	private static final String TAG = TriggerService.class.getSimpleName();
 	private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
-	private static final int POLL_INTERVAL = 1000;
+	private static final int POLL_INTERVAL = 500;
 
 	private LocationManager mLocationManager;
 	private BeaconManager mBeaconManager;
@@ -58,9 +62,7 @@ public class TriggerService extends Service implements LocationListener {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
-
 		final Context text = this;
-
 
 		// Enable the low power bluetooth locatiion service.
 		mBeaconManager = new BeaconManager(this);
@@ -80,21 +82,32 @@ public class TriggerService extends Service implements LocationListener {
 		mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, POLL_INTERVAL, 1.0f, this);
 
-
-		mEstimoteManager = new EstimoteManager(mBeaconManager);
+		mEstimote	Manager = new EstimoteManager(mBeaconManager);
 
 		//config = new TrackerConfiguration();
 		//config.loadFromYaml("/trackerConfig.yml");
+
+		// static looping background sound track.
+		try {
+			MediaPlayer mPlayer = new MediaPlayer();
+			mPlayer.setDataSource(Environment.getExternalStorageDirectory() + "/soundTrack.mp3");
+			mPlayer.prepare();
+			mPlayer.setVolume(0.25f, 0.25f);
+			mPlayer.setLooping(true);
+			mPlayer.start();
+		} catch (Exception e) {
+			Log.e(TAG, "Unable to set background sound track", e);
+		}
 
         // Scale volume percent by max volume.
 		// AudioManager aManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
 		// int maxVolume = aManager.getStreamMaxVolume(AudioManager.STREAM_ALARM);
 
 //		System.err.println("****maxVolume:" + maxVolume);
-		mDynamicST = new DynamicSoundTrack(1.0f);
+//		mDynamicST = new DynamicSoundTrack(1.0f);
 
-		final TriggerLocation loc = new EstimoteLocation(mEstimoteManager, "CC:4A:11:09:A2:C3");
-		mDynamicST.addTrack("/QPAC_LaMer_Test.mp3", loc);
+//		final TriggerLocation loc = new EstimoteLocation(mEstimoteManager, "CC:4A:11:09:A2:C3");
+//		mDynamicST.addTrack("/QPAC_LaMer_Test.mp3", loc);
 
 //        mDynamicST.addTrack("/CreepyKidParkLoop_v01MP3.mp3", -27.511259f, 153.035278f);
 //        mDynamicST.addTrack("/DeadTeacherZoneLoop_v01MP3.mp3", -27.512991f, 153.034958f);
@@ -111,9 +124,39 @@ public class TriggerService extends Service implements LocationListener {
 			public void run() {
 				Looper.prepare();
 				ArrayList<Trigger> triggers = new ArrayList<Trigger>();
-
 				ChainTrigger chain = new ChainTrigger(null);
-				chain.addTrigger(new LocationTrigger(loc, new PlayAudioAction("/12m-monkeybusiness.wav")));
+
+				chain.addTrigger(new DelayedTrigger(30, new PlayAudioAction("/timeTrigger.mp3")));
+
+				TriggerLocation loc = new EstimoteLocation(mEstimoteManager, "CC:4A:11:09:A2:C3");
+				chain.addTrigger(new LocationTrigger(loc, new PlayAudioAction("/station1.mp3")));
+
+				loc = new EstimoteLocation(mEstimoteManager, "F3:13:F9:66:8B:95");
+				chain.addTrigger(new LocationTrigger(loc, new PlayAudioAction("/station2.mp3")));
+
+				loc = new EstimoteLocation(mEstimoteManager, "F5:01:C3:01:18:3E");
+				chain.addTrigger(new LocationTrigger(loc, new PlayAudioAction("/station3.mp3")));
+
+				loc = new EstimoteLocation(mEstimoteManager, "D7:CF:78:0F:4B:E2");
+				chain.addTrigger(new LocationTrigger(loc, new PlayAudioAction("/station4.mp3")));
+
+				ChainTrigger station5 = new ChainTrigger(null);
+				loc = new EstimoteLocation(mEstimoteManager, "EA:83:5B:66:2C:B2");
+				station5.addTrigger(new LocationTrigger(loc, new PlayAudioAction("/station5.mp3")));
+				station5.addTrigger(new DelayedTrigger(135, new Action() {
+					@Override
+					public void trigger() {
+						try {
+							Intent b = new Intent(Intent.ACTION_VIEW, Uri.parse("http://basichuman.com.au"));
+							b.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+							startActivity(b);
+						} catch (Exception e) {
+							Log.e(TAG, "Unable to pop browser: ", e);
+						}
+					}
+				}));
+				chain.addTrigger(station5);
+
 				triggers.add(chain);
 
 				PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
@@ -124,8 +167,8 @@ public class TriggerService extends Service implements LocationListener {
 	                wl.acquire();
 
 	                // Update the sound levels in the dynamic sound track.
-	                mDynamicST.updateLevels();
-	                Toast.makeText(text, Float.toString(loc.distance()), Toast.LENGTH_SHORT).show();
+	                //mDynamicST.updateLevels();
+	                // Toast.makeText(text, Float.toString(loc.distance()), Toast.LENGTH_SHORT).show();
 
 					// For each registered trigger - see if it fires.
 					for (Trigger t : triggers) {
@@ -144,7 +187,7 @@ public class TriggerService extends Service implements LocationListener {
 				wl.release();
 
 				// All done, turn the sound track off.
-				mDynamicST.shutdown();
+				//mDynamicST.shutdown();
 		  }
 		}).start();
 
